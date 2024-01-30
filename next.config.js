7f6bf45a26c247c476e3f8415527c891f03e803a
Jogeahyun <whro815@naver.com>
2024-01-31 00:49:33 +0900

    next.config update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     images:{
         domains:[
             'prod-files-secure.s3.us-west-2.amazonaws.com',
             'images.unsplash.com',
             'www.notion.so'
-        ]
+        ],
+        format: ['image/png', 'image/webp', 'image/jpeg']
     }
 }
 
